57793ebb2a40f1946328824e937714206723d03b
Tiago Deliberali <tiago.deliberali@gmail.com>
2019-08-03 17:59:48 -0300

    file mover working

--- Landmarks/find_files.py
+++ Landmarks/find_files.py
@@ -17,7 +17,7 @@ data.columns = ["id", "path"]
 print("number of files: ", data.shape[0])
 
 # read selected images
-selected_data = pd.read_csv("./Landmarks/top_landmarks_url.csv")
+selected_data = pd.read_csv("./Landmarks/data/top_landmarks_all.csv")
 print("number of selected image files: ", selected_data.shape[0])
 
 # associated selected images with local paths
@@ -25,9 +25,8 @@ merged_data = pd.merge(selected_data, data, on='id', how='inner')
 print("number of selected image found: ", merged_data.shape[0])
 
 # create folders
-landmarks = pd.read_csv("./Landmarks/top_landmarks_counts.csv")
-for index, row in landmarks.iterrows():
-    directory_path = f"{DESTINATION_PATH}{row['landmark_id']}"
+for item in selected_data.landmark_id.unique():
+    directory_path = f"{DESTINATION_PATH}{item}"
     if not os.path.exists(directory_path):
         bashCommand = f"mkdir {directory_path}"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
@@ -36,14 +35,15 @@ for index, row in landmarks.iterrows():
 # Get copied files
 copied_files = []
 for filename in glob.iglob(f"{DESTINATION_PATH}**/*.jpg", recursive=True):
-     copied_files.append([filename[-20:-4], filename])
+     copied_files.append(filename[-20:-4])
+print("number of copied images: ", copied_files.__len__())
 
 # move files to a central place
 count = 0
 for index, row in merged_data.iterrows():
-    if True:
+    if row.id not in copied_files:
         count = count + 1
-        bashCommand = f"cp {row['path']} {DESTINATION_PATH}{row['landmark_id']}/"
+        bashCommand = f"cp {row.path} {DESTINATION_PATH}{row.landmark_id}/"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 print("number of moved images: ", count)
